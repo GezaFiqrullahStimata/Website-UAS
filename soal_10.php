818ab4bdd5a60da32274153a20032f4f2838ad89
Geza Fiqrullah Alamsyah Vitsu Budi Wardana <geza_21510013@stimata.ac.id>
2021-12-31 06:44:20 +0700

    Prak.UAS : soal_10

---
+++ soal_10.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kondisi PHP</title>
+</head>
+<body>
+<?php
+$a = 50;
+$b = 10;
+if($a == $b) {
+     echo "BENAR";
+} else {
+     echo "SALAH";
+}
+?>
+</body>
+</html>
